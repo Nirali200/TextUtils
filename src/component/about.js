b71fcd58f63d2f565e1b706d58dcc10d51d93a01
Nirali200 <darjinirali08@gmail.com>
2023-08-17 16:04:55 +0530

    About modified

--- src/component/about.js
+++ src/component/about.js
@@ -12,7 +12,7 @@ export default function About(props) {
   return (
     <>
     {/* <div className="container my-3" style={myStyle}> */}
-    <h1>{props.heading}</h1>
+     
     <div className="accordion" id="accordionExample" style={myStyle}>
       <div className="accordion-item">
         <h2 className="accordion-header">
